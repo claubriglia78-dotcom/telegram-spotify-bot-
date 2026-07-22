e89e7acdf1cdddfc590b803a7b96791065152e02
claubriglia78-dotcom <claubriglia78@gmail.com>
2026-07-22 12:10:36 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import * as http from 'http';
-
-const port = process.env.PORT || 8000;
-
-http.createServer((_req, res) => {
-  res.write("Bot activo 24/7");
-  res.end();
-}).listen(port);
-
 import { Telegraf } from "telegraf";
 import { execFile } from "child_process";
 import { promisify } from "util";
